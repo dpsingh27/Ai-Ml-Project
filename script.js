c45d511c2f1ed95f8098969699fe3add6728608b
Divya Pratap Singh <110913829+dpsingh27@users.noreply.github.com>
2025-11-03 08:49:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script type="module">
+  import Chatbot from "https://cdn.jsdelivr.net/npm/@denserai/embed-chat@1/dist/web.min.js";
+  Chatbot.init({
+    chatbotId: "chatbot_a8zohm5c3yslwp3qh3kvq",
+  });
+</script> 
